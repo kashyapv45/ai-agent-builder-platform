907959befe87d69317e2b8506c39281ab9d65c5a
BuildTools <unconfigured@null.spigotmc.org>
2026-06-14 15:19:55 -0500

    Bugfix Patch 1.6.14.319

--- app/api/arcjet/route.ts
+++ app/api/arcjet/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const userEmail = "user@example.com"; // Replace with your authenticated user email
-  const decision = await aj.protect(req, { userEmail, requested: 500 }); // Deduct 5 tokens from the bucket
+  const decision = await aj.protect(req, { userId: userEmail, requested: 500 }); // Deduct 5 tokens from the bucket
   console.log("Arcjet decision", decision);
 
   if (decision.isDenied()) {
